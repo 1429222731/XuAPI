315b96fe703dca1c0565941ad3e4d182d4160cc3
超人不会飞 <1429222731@qq.com>
2025-04-17 16:12:00 +0800

    【学习Gateway】 后端 1. 创建项目 xuapi-gateway 2. 学习 Gateway

---
+++ xuapi-gateway/src/test/java/com/charls/xuapigateway/XuapiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charls.xuapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XuapiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
